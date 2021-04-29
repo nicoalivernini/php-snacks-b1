ab60cc8a5a7e51ff87f9fe2330c0365462a89ccc
Nicolò Alivernini <alivernininicolo@gmail.com>
2021-04-29 16:02:09 +0200

    Snack 3 fatto

--- Snack-3/index.php
+++ Snack-3/index.php
@@ -12,6 +12,37 @@
   (ad es 31/01/2007) e come valore un array di post associati a quella data.
   Stampare ogni data con i relativi post. -->
 
+  <?php
+    $messages = [
+      '01/01/2021' => [
+        'title' => 'Post 1',
+        'author' => 'Pippo',
+        'text' => 'Testo del post numero 1 creato da Pippo'
+      ],
+      '02/01/2021' => [
+        'title' => 'Post 2',
+        'author' => 'Pluto',
+        'text' => 'Testo del post numero 2 creato da Pluto'
+      ],
+      '03/01/2021' => [
+        'title' => 'Post 3',
+        'author' => 'Topolino',
+        'text' => 'Testo del post numero 3 creato da Topolino'
+      ],
+    ];
+   ?>
+
+   <?php
+   for ($i = 1; $i < count($messages) + 1 ; $i++) {
+     ?>
+     <div>
+        <?= 'Date:'. ' ' . array_keys($messages)[$i - 1] . '<br>'  ?>
+        <?= 'Title:'. ' ' . $messages['0'.$i.'/01/2021']['title'] . '<br>' ?>
+        <?= 'Author:'. ' ' . $messages['0'.$i.'/01/2021']['author'] . '<br>' ?>
+        <?= 'Text:'. ' ' . $messages['0'.$i.'/01/2021']['text'] . '<br>' ?>
+     </div>
+   <?php } ?>
+
 
 </body>
 </html>
